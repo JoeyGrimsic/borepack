2b814c6e183896d59418c403e3ab98c66aed204d
Joseph Grimsic <golfpro4034@gmail.com>
2024-11-02 23:56:36 -0400

    Update premake5.lua

--- premake5.lua
+++ premake5.lua
@@ -1,45 +1,45 @@
 workspace("borepack")
-configurations({ "Debug", "Release" })
-architecture("x64")
+    configurations({ "Debug", "Release" })
+    architecture("x64")
 
 project("borepack")
-kind("WindowedApp")
-language("C++")
-targetdir("bin/%{cfg.buildcfg}")
+    kind("WindowedApp")
+    language("C++")
+    targetdir("bin/%{cfg.buildcfg}")
 
--- Common include directories
-includedirs({ "deps/glad/include", "deps/glm" })
+    -- Common include directories
+    includedirs({ "deps/glad/include", "deps/glm", "deps/assimp/include", "src" })
 
--- Common files
-files({
-	"src/**.h",
-	"src/**.cpp",
-	"deps/glad/src/glad.cpp",
-})
+    -- Common files
+    files({
+        "src/**.h",
+        "src/**.cpp",
+        "deps/glad/src/glad.cpp",
+    })
 
-filter("configurations:Debug")
-defines({ "DEBUG" })
-symbols("On")
+    filter("configurations:Debug")
+        defines({ "DEBUG" })
+        symbols("On")
 
-filter("configurations:Release")
-defines({ "NDEBUG" })
-optimize("On")
+    filter("configurations:Release")
+        defines({ "NDEBUG" })
+        optimize("On")
 
--- Linux-specific setup
-filter("action:gmake")
-defines({ "LINUX" })
-buildoptions({ "-std=c++17", "-g", "-Wall", "-Wformat" })
-links({ "GL", "glfw" })
-linkoptions({ "`pkg-config --static --libs glfw3`" })
-includedirs({ "`pkg-config --cflags glfw3`" })
+    -- Linux-specific setup
+    filter("system:linux")
+        defines({ "LINUX" })
+        buildoptions({ "-std=c++17", "-g", "-Wall", "-Wformat" })
+        links({ "GL", "glfw", "assimp" })
+        includedirs({ "/usr/include", "/usr/local/include" })
+        libdirs({ "/usr/lib", "/usr/local/lib" })
 
--- Windows-specific setup for Visual Studio
-filter("action:vs2022")
-defines({ "WINDOWS" })
-buildoptions({ "/std:c++17" })
-links({ "opengl32.lib", "glfw3.lib" })
-includedirs({ "deps/glfw/include", "src" })
-libdirs({ "deps/glfw/lib" })
+    -- Windows-specific setup
+    filter("system:windows")
+        defines({ "WINDOWS" })
+        buildoptions({ "/std:c++17" })
+        links({ "opengl32.lib", "glfw3.lib", "assimp.lib" })
+        includedirs({ "deps/glfw/include", "deps/assimp/include", "src" })
+        libdirs({ "deps/glfw/lib", "deps/assimp/lib" })
 
--- Reset filters to avoid affecting other projects
-filter({})
+    -- Reset filters
+    filter({})
